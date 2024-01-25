f1cc4376fea265d494bcd26ae8c5f1dbd8a5fb5b
jngmnj <exorbitance@naver.com>
2024-01-25 17:58:55 +0900

    [2024-01-25] nav item 작성

---
+++ nextjs-market-app/src/app/user/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserPage = () => {
+  return (
+    <div>로그인된 사용자만 볼 수있는 페이지</div>
+  )
+}
+
+export default UserPage
--- nextjs-market-app/src/components/Navbar.tsx
+++ nextjs-market-app/src/components/Navbar.tsx
@@ -2,14 +2,15 @@
 
 import Link from "next/link";
 import { useState } from "react";
+import NavItem from "./NavItem";
 
 const Navbar = () => {
   const [menu, setMenu] = useState(false);
 
   const handleMenu = () => setMenu(!menu);
   return (
-    <nav>
-      <div>
+    <nav className="relataive z-10 w-full bg-orange-500 text-white">
+      <div className="flex items-center justify-between mx-5 sm:mx-10 lg:mx-20">
         
         <div className="flex items-center text-2xl h-14">
           <Link href="/">Logo</Link>
@@ -19,6 +20,13 @@ const Navbar = () => {
         <div className="text-2xl sm:hidden">
           {(menu === false) ? <button onClick={handleMenu}>+</button> : <button onClick={handleMenu}>-</button>}
         </div>
+
+        <div className="hidden sm:block">
+          <NavItem />
+        </div>
+      </div>
+      <div className="block sm:hidden"> 
+        {(menu === false) ? null : <NavItem mobile />}
       </div>
     </nav>
 
