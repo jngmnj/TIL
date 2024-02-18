e44ac71ed9a7f3bd3041daa331e8a18ad8de13c7
jngmnj <exorbitance@naver.com>
2024-02-18 23:48:15 +0900

    [2024-02-18] pagination 작성중

--- nextjs-market-app/src/components/Pagination.tsx
+++ nextjs-market-app/src/components/Pagination.tsx
@@ -1,4 +1,6 @@
+'use client'
 import React from 'react'
+import usePagination from '@lucasmogari/react-pagination';
 
 interface PaginationProps {
     page: number;
@@ -6,9 +8,30 @@ interface PaginationProps {
     perPage: number;
 }
 const Pagination = ({ page, totalItems, perPage }: PaginationProps) => {
+
+  const {getPageItem, totalPages} = usePagination({
+    totalItems: totalItems,
+    page: page,
+    itemsPerPage: perPage,
+    maxPageItems: 3
+  });
+
+  const firstPage = 2;
+  const nextPage = Math.min(page + 1, totalPages);
+  const prevPage = Math.max(page - 1, firstPage); // 둘중 큰게 prevPage
+
+  const arr = new Array(totalPages + 2); // 
+
+  console.log('getPageItem',getPageItem)
+  console.log('totalPages', totalPages)
+
+  
   return (
     <div>
-        
+        {[...arr].map((_, index) => {
+          console.log("test")
+        })
+        }
     </div>
   );
 };
--- nextjs-market-app/src/helpers/dayjs.ts
+++ nextjs-market-app/src/helpers/dayjs.ts
@@ -2,20 +2,13 @@ import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import 'dayjs/locale/ko';
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 4eeb43f2dac7bd3bab12984b82b8927911708539
 dayjs.extend(relativeTime);
 dayjs.locale('ko');
 
 export function fromNow(time: string | Date) {
     return dayjs(time).fromNow()
-<<<<<<< HEAD
 }
 
 export function formatTime(time: string | Date, format= 'YYYY.MM.DD h:mm A') {
     return dayjs(time).format(format);
-=======
->>>>>>> 4eeb43f2dac7bd3bab12984b82b8927911708539
 }
