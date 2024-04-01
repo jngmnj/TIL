44c2749f3a651edaf39b1191280d5a6b833fc668
jngmnj <exorbitance@naver.com>
2024-04-01 18:02:11 +0900

    [MOD] 틱택토 완성

--- TIL_python/python-basic/python13-14.py
+++ TIL_python/python-basic/python13-14.py
@@ -19,57 +19,144 @@ class Tic_Tac_Toe:
     def select_first_player(self) : 
         if random.randint(0, 1) == 0 : # randint(0, n): 0부터 n까지 랜덤정수
             return 'X'
+        else :
+            return 'O'
         
     # 기호 표시
     def mark_spot(self, row, col, player) : 
+        self.board[row - 1][col - 1] = player # 
 
     # 승리 상태 확인
     # is~ 메서드: return boolean 
     def is_win(self, player) : 
+        n = len(self.board) # board가 가리키는 리스트의 원소 개수 추출
 
-       # 행 확인
-       # 열 확인 
-       # 대각선 확인
+        # 행 확인
+        for i in range(n) :
+            win = True
+            for j in range(n) :
+                if self.board[i][j] != player :# 다른게 나오면 승리X
+                    win = False
+                    break
+            if win == True :
+                return win
+            
+        # 열 확인 
+        for i in range(n) :
+            win = True
+            for j in range(n) :
+                if self.board[j][i] != player : # 다른게 나오면 승리X
+                    win = False
+                    break
+            if win == True :
+                return win
 
-    # 잔여 빈칸 여부 확인
+        # 대각선 확인: 중첩반복 필요 없음
+        win = True
+        for i in range(n) :
+            if self.board[i][i] != player:
+                win = False
+                break
+        if win == True:
+            return win
+        
+        return False
+
+    # 잔여 빈칸 여부 확인: 한칸씩 확인한 후 *가 있으면 false 리턴
     def is_board_full(self) :
+        for row in self.board:
+            for item in row:
+                if item == '*':
+                    return False
+        return True
 
     # 플레이어 변경
     def next_player(self, player) :
+        if player == '0' :
+            return 'X'
+        else :
+            return 'O'
 
+        # 이렇게 간소화해서 작성가능 return 'X' if player == '0' else '0' 
 
     # 현재 게임판 상태 츌력
     def show_board(self) :
+        for row in self.board:
+            for item in row :
+                print(item, end = " ") # 줄바꿈 X
+            print()
+
 
     # 게임 시작
     def start(self) :
 
         # 새 게임판 생성
+        self.create_board()
+        self.show_board()
 
         # 첫 플레이어 선택
+        player = self.select_first_player()
 
         # 게임 루프 시작
+        while True :
 
             # 다음 플레이어 안내
-
+            if player == 'X' :
+                print("컴퓨터 차례입니다.")
+            else :
+                print("사용자 차례입니다.")
+            
             # 현재 게임판 상태 출력
+            self.show_board()
 
             # 사용자 입력 대기, 컴퓨터일 경우 랜덤 위치 반환
+            
+
+            if player == 'X' : # 컴퓨터이면
+                while True : 
+                    row, col = random.randint(1, 3), random.randint(1,3) # 이미 마킹된부분이라면 둘수 없게
+                    if self.board[row][col] == '*' : 
+                        break
+
+                print("컴퓨터가 행 "+ str(row)+", 열 "+str(col)+"을 선택했습니다. ")
+                print()
+            else :
+                # 한번에 입력받기: 예) 1 3
+                # 두개의 값은 map으로 받고 list 변환 -> 각각 할당됨
+                row, col = list(map(int, input("선택한 빈칸의 위치를 입력하세요: ").split())) 
+                print("사용자가 행 "+ str(row)+", 열 "+str(col)+"을 선택했습니다. ")
+                print()
 
             # row, col 입력값이 0, 0인 경우 게임 종료
+            if row == 0 and col == 0 :
+                break
 
             # 입력된 위치 표시
+            self.mark_spot(row, col, player)
+            self.show_board()
 
             # 현재 플레이어가 이겼는지 확인
+            if self.is_win(player) == True :
+                if player == 'X' :
+                    print("컴퓨터가 이겼습니다. 다시 도전하세요. ")
+                else :
+                    print("사용자가 이겼습니다. 축하합니다. ")
+                break
 
-            # 게임판 가득참 확인
+            # 게임판 가득참 확인, 빈칸여부
+            if self.is_board_full() == True :
+                print("무승부입니다. 다시 도전하세요. ")
+                break
 
             # 플레이어 변경
+            player = self.next_player(player)
 
         # 최종 게임판 출력
+        print()
+        self.show_board()
 
 
 # 게임 생성
-TTT = Tic_Tac_Toe():
+TTT = Tic_Tac_Toe()
 # 게임 시작
-TTT.start() :
+TTT.start()
--- TIL_python/python-turtle/midtermAssignment.py
+++ TIL_python/python-turtle/midtermAssignment.py
@@ -1,21 +1,17 @@
 # 2024-03-26
 # 조건문 반복문 함수 하나 이상씩 사용하여 터틀로 그림그리기
+# 크기가 점점 커지면서 opacity가 줄어드는 컬러감(rgba -> rgb 변환 필요)
 
-# ideas
-# 서클 그리는데 컬러, 펜굵기 어떤 지점에서 다르게 (펜굵기와 컬러는 불규칙적으로 )
-# 떨어지는 빗방울 transition animation
-# fade out 되거나
-# 크기가 점점 커지면서 없어지거나 
 import turtle as t
 from random import randint
 
 # 화면 초기화
 t.clearscreen()
 
-# background color
+# background color 설정
 t.bgcolor('white')
 
-# color mode RGB
+# color mode RGB로 변경
 t.colormode(255);
 
 # 팬톤 RGB color palette
@@ -35,16 +31,20 @@ def rgba(rgb, a):
 
 
 # RGBA 색상을 RGB 형식으로 변환하는 함수
+# a = 알파채널값. 투명도. 
+# RGB는 알파채널이 없기때문에 투명도를 색상에 반영하려면 
+# (1-a) * 255에 a*(r|g|b)를 더한다. 
 def rgba_to_rgb(rgba_color):
     r, g, b, a = rgba_color
     return (int((1 - a) * 255 + a * r), int((1 - a) * 255 + a * g), int((1 - a) * 255 + a * b))
 
 
 # radius of the circle 
-r = 10
+r = 8
+
 
 # 속도 설정
-t.speed(200)
+t.speed(2000)
   
 # 퍼져나가면서 점점 옅어지기 
 # opacity는 1에서 0까지 .1씩 작아짐
@@ -55,12 +55,16 @@ def drawRain():
     # 좌표
     x = randint(-250, 250)
     y = randint(-250, 250)
+    # 그림 색상이 팬톤 컬러팔레트에서 0부터 6개 
     randomColor = colors[randint(0,6)]
     t.up() 
     t.goto(x,y)
     t.down()
     
     for i in range(10, -1, -1): 
+        # 그림 하나에 크기가 작은것부터 큰것을 그리는데
+        
+        # 하나 그릴때마다 색상이 점점 옅어지게된다.
         t.pencolor(rgba_to_rgb(rgba(randomColor, i/10)))
         t.circle((11 - i)*r) 
 
@@ -70,6 +74,7 @@ def drawRain():
         t.sety(newY)
         t.down()
 
+# 30개 그린다. 
 for i in range(30):
     drawRain()
 
